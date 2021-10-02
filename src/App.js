76668fd47ccbf9fe209653fffa1eacff0a9bae2f
CarlosRocha2409 <carlos.eduardo.rocha.silva@gmail.com>
2021-10-02 13:14:21 -0600

    test 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App" style={{ height: "100vh" }}>
       <iframe
+        title="map"
         src="https://www.google.com/maps/d/embed?mid=1Xdh7T-f8WNp8Obs6J_6DU5uYT32FzK_x"
         width="100%"
         height="100%"
