088e6a2e25e3a63844605203c41df9f812d6d50d
CarlosRocha2409 <carlos.eduardo.rocha.silva@gmail.com>
2021-10-02 13:09:43 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: "100vh" }}>
+      <iframe
+        src="https://www.google.com/maps/d/embed?mid=1Xdh7T-f8WNp8Obs6J_6DU5uYT32FzK_x"
+        width="100%"
+        height="100%"
+      ></iframe>
     </div>
   );
 }
